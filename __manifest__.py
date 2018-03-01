bb9b9716f3041cc412db3100bd0227cfea4d6cf0
Alfie Qashwa <alfieqashwa@gmail.com>
2018-03-01 17:51:33 +0700

    initial commit

---
+++ __manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Open Academy",
+    'summary': """Manage trainings / Exercise Purpose Only""",
+    'description': """
+        Open Academy module for managing trainings:
+            - training courses
+            - training sessions
+            - attendees registration
+    """,
+
+    'author': "Alfie Qashwa",
+    'website':"https://alfieqashwa.github.io",
+    'category': 'Test',
+    'version': '0.1',
+
+    'depends':['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/templates.xml',
+        'views/openacademy.xml',
+    ],
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
